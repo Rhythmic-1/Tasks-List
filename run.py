0a78995a8e2dd971fa5070ba3f9d0aedf06267a7
Subhraneel Saha <87231338+Rhythmic-1@users.noreply.github.com>
2023-10-11 18:03:04 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import webbrowser
 
 # Specify the path to your HTML file
-html_file_path = '/home/rhythmic/Downloads/ccassignment/tasks-list/index.html'
+html_file_path = '/Tasks-List/index.html'
 
 # Open the HTML file in the default web browser
 webbrowser.open('file://' + html_file_path)
