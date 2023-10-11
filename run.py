8db9ded2cc7c90a17602ff9ebeacf137b60c3e17
Subhraneel Saha <87231338+Rhythmic-1@users.noreply.github.com>
2023-10-11 18:05:54 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import webbrowser
 
 # Specify the path to your HTML file
-html_file_path = '/Tasks-List/index.html'
+html_file_path = 'index.html'
 
 # Open the HTML file in the default web browser
 webbrowser.open('file://' + html_file_path)
