1938cc259835c3e89f9694c8e7ef8108a7ebebeb
Subhraneel Saha <87231338+Rhythmic-1@users.noreply.github.com>
2023-10-11 13:44:54 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# Specify the path to your HTML file
+html_file_path = '/home/rhythmic/Downloads/ccassignment/tasks-list/index.html'
+
+# Open the HTML file in the default web browser
+webbrowser.open('file://' + html_file_path)
